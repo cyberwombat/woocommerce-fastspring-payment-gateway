72ae974454894075b492f91953e290212d8f3c06
Yashua <yashua@urbanlotus.com>
2018-05-18 15:31:40 -0700

    Nginx shim

--- includes/class-wc-gateway-fastspring-handler.php
+++ includes/class-wc-gateway-fastspring-handler.php
@@ -3,6 +3,20 @@ if (!defined('ABSPATH')) {
   exit;
 }
 
+// Polyfill for nginx
+if (!function_exists('getallheaders')) { 
+  function getallheaders() {
+    $headers = []; 
+    foreach ($_SERVER as $name => $value) { 
+      if (substr($name, 0, 5) == 'HTTP_') { 
+        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+      } 
+    } 
+    return $headers; 
+  } 
+} 
+
+
 /**
  * Base class to handle ajax and webhook request from FastSpring.
  *
